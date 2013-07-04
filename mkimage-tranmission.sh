c511e158b8d3a43bc2b73f853cc5907682163f1d
Ivan Krasin <imkrasin@gmail.com>
2013-07-04 17:12:49 +0000

    Run ssh daemon

--- mkimage-tranmission.sh
+++ mkimage-tranmission.sh
@@ -2,9 +2,9 @@
 
 set -ue
 
-if [ $# -ne 4 ]
+if [ $# -ne 5 ]
 then
-    echo "Usage: ./mkimage-transmission.sh <username> <password> <port> <peer_port>"
+    echo "Usage: ./mkimage-transmission.sh <username> <password> <port> <peer_port> <ssh_port>"
     exit 1
 fi
 
@@ -12,6 +12,7 @@ readonly USERNAME=$1
 readonly PASSWORD=$2
 readonly PORT=$3
 readonly PEER_PORT=$4
+readonly SSH_PORT=$5
 
 docker rmi transmission || echo "Old transmission image not found, so nothing to delete"
 
@@ -21,6 +22,7 @@ sed -i "s/@USERNAME@/$USERNAME/g" $DOCKERFILE_TMP
 sed -i "s/@PASSWORD@/$PASSWORD/g" $DOCKERFILE_TMP
 sed -i "s/@PORT@/$PORT/g" $DOCKERFILE_TMP
 sed -i "s/@PEER_PORT@/$PEER_PORT/g" $DOCKERFILE_TMP
+sed -i "s/@SSH_PORT@/$SSH_PORT/g" $DOCKERFILE_TMP
 
 docker build -t transmission - < $DOCKERFILE_TMP
 echo $DOCKERFILE_TMP
